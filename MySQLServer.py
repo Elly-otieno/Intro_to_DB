84986ba7e81d2886ed03d15ea500a818a9e1906d
Elly-otieno <okothelli@gmail.com>
2025-10-19 22:30:34 +0300

    fixes

--- MySQLServer.py
+++ MySQLServer.py
@@ -26,24 +26,39 @@ try:
     print("Database 'alx_book_store' created successfully!")
     
     mycursor.execute("USE alx_book_store;")
-    print("Database 'alx_book_store' selected successfully!")
+    print("Database 'alx_book_store' created successfully!")
 except Exception as e:
     print(f'An error occured: {e}')
 
 
-with open('task_2.sql', 'r') as sql_file:
-    sql_commands = sql_file.read()
+def run_sql_file(filename):
+    with open(filename, 'r') as sql_file:
+        sql_commands = sql_file.read()
 
 # split commands by semicolon and execute each
 
-for command in sql_commands.split(';'):
-    command = command.strip()
-    if command:
-        try:
-            mycursor.execute(command)
-        except Exception as e:
-            print(f"Error executing command: {command}\n{e}")
+    for command in sql_commands.split(';'):
+        command = command.strip()
+        if command:
+            try:
+                mycursor.execute(command)
 
+                # If it's a SELECT query, fetch and print results
+                if command.upper().startswith("SELECT"):
+                    _ = mycursor.fetchall() 
+                    results = mycursor.fetchall()
+                    for row in results:
+                        print(row)
+
+            except Exception as e:
+                print(f"Error executing command: {command}\n{e}")
+
+
+run_sql_file('task_2.sql')
+run_sql_file('task_3.sql')
+run_sql_file('task_4.sql')
+run_sql_file('task_5.sql')
+run_sql_file('task_6.sql')
 # Close connection to the databasse  
 mycursor.close()
 mydb.close()
